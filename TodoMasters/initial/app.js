cfb137097e31be95912ac21586099560c8f8caf8
fthe <lcslianandfthe.cs@gmail.com>
2024-12-08 21:40:50 +0900

    fix: fix ctrl + z not working

--- TodoMasters/initial/app.js
+++ TodoMasters/initial/app.js
@@ -37,7 +37,7 @@ document.addEventListener('keydown', function (event) {
 });
 
 document.addEventListener('keydown', function (event) {
-  if (event.ctrlkey && event.key === 'z') {
+  if (event.ctrlKey && event.key === 'z') {
     event.preventDefault();
     console.log("undo is called")
     // const cmd = new Command(Commands.UNDO);
